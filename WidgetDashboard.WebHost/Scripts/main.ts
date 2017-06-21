910b33bef27c0562846801660bee5472ff30352d
Jon Coello <joncoello@gmail.com>
2017-06-21 14:48:41 +0100

    duplicate add new widget code to allow loading from layout - needs refactoring

--- WidgetDashboard.WebHost/Scripts/main.ts
+++ WidgetDashboard.WebHost/Scripts/main.ts
@@ -44,12 +44,34 @@ $('#toolbox .toolbox-item').click(function () {
 
 $('#saveLayout').click(function () {
 
-    alert('save');
+    WidgetManager.Instance.saveLayout();
 
 });
 
 $('#loadLayout').click(function () {
 
-    alert('load');
+    // dashboard grid
+    var grid = $('#mainArea .grid-stack').data('gridstack');
 
+    WidgetManager.Instance.loadLayout().forEach(w => {
+        
+        //get template
+        var templateElement = $(document.getElementsByName(w.name)[0]);
+        var template = templateElement.html();
+
+        // wrapper html for gridstack component
+        var widgetMarkup = "<div class=\"grid-stack-item\"><div class=\"grid-stack-item-content\"><div class=\"remove-widget\"><i class=\"fa fa-trash-o\"></i></div>" + template + "</div></div>";
+        
+        // gridstack api call
+        var widget = grid.addWidget(widgetMarkup, Number(w.x), Number(w.y), Number(w.width), Number(w.height));
+        var elementToUse = $(widget).find('.widget').first(); // requires widget developer to class their html with .widget
+
+        $(widget).find('.remove-widget').click(function () {
+            grid.removeWidget(widget.get(0));
+        });
+
+        WidgetManager.Instance.createWidget(elementToUse.get(0), templateElement.attr("name"));
+
+    });
+    
 });
